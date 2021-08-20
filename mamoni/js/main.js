b0258aac51c233f71032092a7309ec1b29c121bd
marioizdendy <89105851+marioizdendy@users.noreply.github.com>
2021-08-20 16:01:53 +0300

    Add files via upload

---
+++ mamoni/js/main.js
@@ -0,0 +1,9 @@
+menu.onclick = function myFunction() {
+    var x = document.getElementById('topnav');
+
+    if (x.className === 'nav') {
+        x.className += "responsive";
+    } else {
+        x.className = 'nav';
+    }
+}
